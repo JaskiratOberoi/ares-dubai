3d3db868db4bac072c9f3a2d8d6d60a9eb07d09f
Jaskirat Oberoi <jso101196@gmail.com>
2024-08-17 00:44:12 +0530

    added heartbeat loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
+  const Size = window.innerWidth / 10;
+  console.log(Size);
   return (
     <div className="app">
       <div className="title new-amsterdam-regular">
         ARES DUBAI
         <div className="cs">COMING SOON</div>
+        <div className="heartbeat">
+          <l-cardio size={Size} stroke="5" speed="2" color="#d1b464"></l-cardio>
+        </div>
       </div>
     </div>
   );
