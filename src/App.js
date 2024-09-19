8fdcb956bdcea7ac1fbf3cad6a46b788a9254bb8
Jaskirat Oberoi <jso101196@gmail.com>
2024-09-19 17:34:50 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from "react";
+
 function App() {
   const Size = window.innerWidth / 10;
-  console.log(Size);
+  const Stroke = Size * 0.05;
   return (
     <div className="app">
       <div className="title new-amsterdam-regular">
         ARES DUBAI
         <div className="cs">COMING SOON</div>
         <div className="heartbeat">
-          <l-cardio size={Size} stroke="5" speed="2" color="#d1b464"></l-cardio>
+          <l-cardio
+            size={Size}
+            stroke={Stroke}
+            speed="2"
+            color="#d1b464"
+          ></l-cardio>
         </div>
       </div>
     </div>
